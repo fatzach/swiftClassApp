3795fcdec441685dcd4095687e6be14c6c45b799
Zach Fletcher <zacharyafletcher@gmail.com>
2017-03-04 23:43:50 -0500

    First commit for app class.

--- Fundamentals-4/ViewController.swift
+++ Fundamentals-4/ViewController.swift
@@ -10,9 +10,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var hwLabel: UILabel!
+
+    var tapCount = 0
+    
+    @IBAction func buttonPressed(_ sender: Any) {
+    
+        tapCount = tapCount + 1
+        print(tapCount)
+        if tapCount >= 10 {
+            hwLabel.text = "You tapped the button 10 times!"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
     }
 
     override func didReceiveMemoryWarning() {
